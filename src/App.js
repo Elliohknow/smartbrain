e0ee42e4d2e9d48dc0504baebd1ec1704c9e5c63
Elliott Jones <elliottohj44@gmail.com>
2018-12-17 02:50:32 -0500

    fix final fetch bug

--- src/App.js
+++ src/App.js
@@ -95,7 +95,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://serene-oasis-65374.herokuapp.com:3000/image', {
+          fetch('https://serene-oasis-65374.herokuapp.com/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
